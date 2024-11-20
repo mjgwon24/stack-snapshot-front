a3db6141ac497ca1f3cd55de5c88eb3a723022b7
nanaring1 <adelya1234@naver.com>
2024-11-21 01:42:22 +0900

    프론트:메인페이지사진롤링

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,8 @@
 import React from "react";
 import imageSrc from '../images/icons/stack_dev_logo1.png'; 
-import imageSrc2 from '../images/icons/유형2.png'; 
-import imageSrc3 from '../images/icons/유형3.png'; 
-import imageSrc1 from '../images/icons/유형1.png'; 
-import imageSrc4 from '../images/icons/유형4.png'; 
-import imageSrc5 from '../images/icons/유형5.png';
+import imageSrc3 from '../images/icons/photo3.png'; 
+import imageSrc4 from '../images/icons/photo4.png'; 
+
 import {useNavigate} from 'react-router-dom';
 
 /**
@@ -19,30 +17,86 @@ const MainPage = () => {
     <div className="min-h-screen relative" style={{ overflow: 'hidden' }}>
       
       {/* 애니메이션 이미지 전체 화면 배경 섹션 */}
-      <div className="image-container" style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: -1 }}>
-        {/* 그룹 1: 유형2와 유형3 */}
-        <div style={{ position: 'absolute', right: '20%', animation: 'slideUp 10s linear infinite' }}>
-          <img src={imageSrc2} alt="유형2" style={{ width: '450px', height: '600px' }} />
-        </div>
-        <div style={{ position: 'absolute', right: '20%', animation: 'slideUp 10s linear infinite 4s' }}>
-          <img src={imageSrc3} alt="유형3" style={{ width: '450px', height: '600px' }} />
-        </div>
+      <div
+  className="image-container"
+  style={{
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    zIndex: -1,
+  }}
+>
+  {/* 첫 번째 이미지 */}
+  <div
+    style={{
+      position: "absolute",
+      right: "20%",
+      animation: "slideUp 45s linear infinite",
+    }}
+  >
+    <img
+      src={imageSrc3}
+      alt="유형2"
+      style={{ width: "327px", height: "3000px" }}
+    />
+  </div>
 
-        {/* 그룹 2: 유형1 */}
-        <div style={{ position: 'absolute', right: '0%', animation: 'slideUp 10s linear infinite' }}>
-          <img src={imageSrc1} alt="유형1" style={{ width: '327px', height: '476px' }} />
-        </div>
 
-        {/* 그룹 3: 유형4 */}
-        <div style={{ position: 'absolute', right: '0%', animation: 'slideUp 10s linear infinite 3s' }}>
-          <img src={imageSrc4} alt="유형4" style={{ width: '327px', height: '476px' }} />
-        </div>
 
-        {/* 그룹 4: 유형5 */}
-        <div style={{ position: 'absolute', right: '0%', animation: 'slideUp 10s linear infinite 6s' }}>
-          <img src={imageSrc5} alt="유형5" style={{ width: '327px', height: '650px' }} />
-        </div>
-      </div>
+
+
+{/* 애니메이션 효과 추가 */}
+<style>
+  {`
+   @keyframes slideUp {
+      0% {
+        transform: translateY(100vh); /* 화면 맨 아래에서 시작 */
+        opacity: 1;
+      }
+      100% {
+        transform: translateY(-3000px); /* 화면 위로 나가서 사라짐 */
+        opacity: 1;
+      }
+    }
+  `}
+</style>
+
+
+  {/* 두 번째 이미지 */}
+  <div
+    style={{
+      position: "absolute",
+      right: "0%",
+      animation: "slideUp1 45s linear infinite",
+    }}
+  >
+    <img
+      src={imageSrc4}
+      alt="유형1"
+      style={{ width: "246px", height: "2900px" }}
+    />
+  </div>
+</div>
+
+{/* 애니메이션 효과 추가 */}
+<style>
+  {`
+    
+    @keyframes slideUp1 {
+      0% {
+        transform: translateY(100vh); /* 화면 맨 아래에서 시작 */
+        opacity: 1;
+      }
+      100% {
+        transform: translateY(-2900px); /* 화면 위로 나가서 사라짐 */
+        opacity: 1;
+      }
+    }
+  `}
+</style>
+
 
       {/* 상단 컨텐츠 섹션 */}
       <div className="content-container" style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1, pointerEvents: 'none' }}>
